5d77cf0e34ce361d40093adac51a58fc3ad798f3
Ricards Rudups <ricardsrudups99@gmail.com>
2025-03-29 18:12:18 +0000

    counter works

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ let cookiesPerSecond = 1;
 // 1 second increment
 setInterval(function () {
 cookieCount = cookieCount + cookiesPerSecond;
+totalPokemon.textContent = `Total Pokemon: ${cookieCount}`;
 console.log(cookieCount);
 }, 1000);
 
 // render the change on screen
-const totalPokemon = document.getElementById("total-pokemon");
-totalPokemon.textContent = `Total Pokemon: ${cookieCount}`;
+const totalPokemon = document.getElementById("totalpokemon")
