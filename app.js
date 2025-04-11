4bb48267c7fdf1b6b6d587832864b8a2f5cac655
Ricards Rudups <ricardsrudups99@gmail.com>
2025-04-11 20:17:38 +0100

    I changed the idea to a cookie and I think I put the items into my own array.

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ console.log("connection successfull...")
 let cookieCount = 0;
 let cookiesPerSecond = 1;
 
+//declare empty array for the api to use
+
+
 // 1 second increment
 setInterval(function () {
 cookieCount = cookieCount + cookiesPerSecond;
-totalPokemon.textContent = `Total Pokemon: ${cookieCount}`;
+totalPokemon.textContent = `Total Cookies: ${cookieCount}`;
 console.log(cookieCount);
 }, 1000);
 
@@ -17,8 +20,25 @@ const totalPokemon = document.getElementById("totalpokemon")
 const button = document.getElementById("cookie-increment-button")
 button.addEventListener("click", function () {
     cookieCount = cookieCount + 1;
-    totalPokemon.textContent = `Total Pokemon: ${cookieCount}`;
+    totalPokemon.textContent = `Total Cookies: ${cookieCount}`;
 });
-//render pokemon per second
+//render cookies per second
 const currentpps = document.getElementById("current-pps")
-currentpps.textContent = `Pokemon Per Second: ${cookiesPerSecond}`;
+currentpps.textContent = `Cookies Per Second: ${cookiesPerSecond}`;
+
+//fetching from upgrades api
+const upgradesbox = document.getElementById("upgrades-container")
+const shopUpgradesArray = [];
+async function getShopUpgrages() {
+    const result = await fetch("https://cookie-upgrade-api.vercel.app/api/upgrades");
+    const data = await result.json()
+    console.log("api test", data);
+    data.forEach(item => {
+        shopUpgradesArray.push(item);
+    });
+    return data;
+    
+}
+
+
+getShopUpgrages();
